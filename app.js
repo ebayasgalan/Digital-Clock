923c18a7f055b354129f896be6a3970cbdfe2dda
Eric Bayasgalan <ewiz1020@gmail.com>
2019-04-24 14:06:08 -0700

    some change

---
+++ app.js
@@ -0,0 +1,6 @@
+const nowDate = new Date();
+const hours = nowDate.getHours();
+const minutes = nowDate.getMinutes();
+const seconds = nowDate.getSeconds();
+
+console.log(document.getElementById("minute"));
