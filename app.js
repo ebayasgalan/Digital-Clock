ac99e8266e51da7e388681da37f00f9ddc32fab6
Eric Bayasgalan <ewiz1020@gmail.com>
2019-04-24 20:13:29 -0700

    finished the javascript part

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const nowDate = new Date();
-const hours = nowDate.getHours();
-const minutes = nowDate.getMinutes();
-const seconds = nowDate.getSeconds();
+function clock() {
+  const nowDate = new Date();
+  let hours = nowDate.getHours();
+  let minutes = nowDate.getMinutes();
+  let seconds = nowDate.getSeconds();
 
-console.log(document.getElementById("minute"));
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = ": " + minutes;
+  document.getElementById("second").innerHTML = ": " + seconds;
+}
+
+setInterval(clock, 100);
